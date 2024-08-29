8498e594b42005b427dd6fb8d1e74f1e6e04d26b
Faiza2850 <saffafatima06@gmail.com>
2024-08-29 20:05:58 +0500

    PORTFOLIO

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Navbar } from './components/Navbar'
 import { Home } from './components/Home'
+import {Experience} from './components/Experience'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +16,9 @@ function App() {
       
       <div className="container">
       <Home/>
+
+      <Experience/>
+
 
       </div>
       
--- portfolio/src/components/Experience.jsx
+++ portfolio/src/components/Experience.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import experience from './data/experience.json'
 
 export const Experience = () => {
   return (
-    <div>Experience</div>
+    <>
+    <div className='container ex'>
+      <h1>EXPERIENCE</h1>
+      {
+        experience.map((data)=>{
+          return(
+            <>
+            <div key={data.id} className='ex-items'>
+              <div className="left">
+                <img src="" alt="" />
+              </div>
+              <div className="right">
+                <h2>{data.role}</h2>
+              </div>
+
+
+            </div>
+            </>
+          )
+        }
+      )
+      }
+    </div>
+    </>
   )
 }
