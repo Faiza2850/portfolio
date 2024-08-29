13c8f779d811934a5cebfe580bcee45a115cbd76
Faiza2850 <saffafatima06@gmail.com>
2024-08-30 02:41:31 +0500

    Portfolio

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import { Navbar } from './components/Navbar'
 import { Home } from './components/Home'
 import {Experience} from './components/Experience'
+import { Skills } from './components/Skills'
+import {Projects} from './components/Projects'
 
 
 function App() {
@@ -18,7 +20,8 @@ function App() {
       <Home/>
 
       <Experience/>
-
+      <Skills/>
+      <Projects/>
 
       </div>
       
--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import skills from "./data/skills.json";
 
 export const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
+    <>
+      <div className="container skills">
+        <h1>SKILLS</h1>
+        {skills.map((data) => (
+          <>
+            <div className="items" key={data.id}>
+              <div className="item">
+                <img src={`/assets/${data.imageSrc}`} alt="" />
+                <h3> {data.title}</h3>
+              </div>
+            </div>
+          </>
+        ))}
+      </div>
+    </>
+  );
+};
