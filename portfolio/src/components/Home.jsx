b85213c3603a42009f418afedf0958a909694a89
Faiza2850 <saffafatima06@gmail.com>
2024-08-29 13:56:54 +0500

    PORTFOLIO

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React from "react";
+import pdf from "../pdf/resume.pdf";
+import hero from './data/hero.json';
+
 
 export const Home = () => {
   return (
     <>
-    <div className="container">
+      <div className="container home">
         <div className="left">
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur sunt, eius repudiandae, corrupti illum facere nisi temporibus ab a aliquid reprehenderit hic? Fugit vel sit perferendis rerum quo assumenda porro.
-            </h1>
-            <a href="" className="btn btn-outline-warning">Download Resume</a>
+          <h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+            sunt, eius repudiandae, corrupti illum facere nisi temporibus ab a
+            aliquid reprehenderit hic? Fugit vel sit perferendis rerum quo
+            assumenda porro.
+          </h1>
+          <a
+            href={pdf}
+            download="Resume.pdf"
+            className="btn btn-outline-warning"
+          >
+            Download Resume
+          </a>
         </div>
-        <div className="right"></div>
-    </div>
+        <div className="right">
+          <div className="img">
+            <img src={`/assets/${hero.imgSrc}`}alt=""/>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
