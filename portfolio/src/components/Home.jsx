3b7184723c8c597c571c8a30fa6ef20d57d92753
Faiza2850 <saffafatima06@gmail.com>
2024-08-29 10:27:19 +0500

    PORTFOLIO

---
+++ portfolio/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <>
+    <div className="container">
+        <div className="left">
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur sunt, eius repudiandae, corrupti illum facere nisi temporibus ab a aliquid reprehenderit hic? Fugit vel sit perferendis rerum quo assumenda porro.
+            </h1>
+            <a href="" className="btn btn-outline-warning">Download Resume</a>
+        </div>
+        <div className="right"></div>
+    </div>
+    </>
+  )
+}
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className="container nav_bar">
+        <div className='left'>Portfolio</div>
+        <div className="right">
+            <a href="" className="nav_items">Home</a>
+            <a href="" className="nav_items">Experience</a>
+            <a href="" className="nav_items">Skills</a>
+            <a href="" className="nav_items">Projects</a>
+            <a href="" className="nav_items">Contact</a>
+        </div>
+    </div>
+  )
+}
