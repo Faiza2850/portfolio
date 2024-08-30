70cdbabace88c03f313f04824324786a9bc0af43
Faiza2850 <saffafatima06@gmail.com>
2024-08-30 17:22:43 +0500

    feat: add experience

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,31 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Navbar } from './components/Navbar'
-import { Home } from './components/Home'
-import {Experience} from './components/Experience'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
+import {Experience } from "./components/Experience";
+import { Skills } from "./components/Skills";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Navbar/>
-      
+      <Navbar />
       <div className="container">
-      <Home/>
+        <Home />
 
-      <Experience/>
-
-
+       <Experience/>
+        <Skills />
+       
       </div>
-      
-    
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
